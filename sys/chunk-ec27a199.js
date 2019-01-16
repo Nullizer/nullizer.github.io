fb4d9875aeff43f5003eebb467281935c020eaa7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-16 10:08:07 +0000

    Deploy Nullizer/nullizer.github.io to github.com/Nullizer/nullizer.github.io.git:master

--- sys/chunk-ec27a199.js
+++
@@ -1,1 +0,0 @@
-System.register(["./chunk-ff03ac4b.js"],function(t,n){"use strict";var e,u;return{setters:[function(t){e=t.k,u=t.b}],execute:function(){t("default",function(){const[t,n]=e(!0);return u("button",{onClick:()=>n(!t)},t?"ON":"OFF")})}}});
